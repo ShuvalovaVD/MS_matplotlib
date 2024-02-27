971683acc07f26e61524afb571e7ac8bbacadb24
Shuvalova Valeria <114807973+ShuvalovaVD@users.noreply.github.com>
2024-02-27 18:46:50 +0400

    Create line_graph_frequency_polygon_y.py

---
+++ line_graph_frequency_polygon_y.py
@@ -0,0 +1,22 @@
+# Полигон частот для Y
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FixedLocator
+# набор данных
+x_all = [6.38, 10.57, 14.75, 18.93, 23.11, 27.29, 31.47, 35.65, 39.83, 44.01]
+y_all = [0.02, 0.03, 0.04, 0.03, 0.02, 0.01, 0.03, 0.03, 0.01, 0.02]
+# создание фигуры
+fig = plt.figure(figsize=(10, 6))
+p = fig.add_subplot()
+# линейный график
+p.plot(x_all, y_all, color="red", marker="o")
+# легенда
+plt.title("Полигон частот для Y")
+plt.ylabel("n_i/(n*w)")
+plt.xlabel("y")
+# сетка
+x_all_unique, y_all_unique = list(set(x_all)), list(set(y_all))
+p.xaxis.set_major_locator(FixedLocator(x_all_unique))
+p.yaxis.set_major_locator(FixedLocator(y_all_unique))
+p.grid()
+# вывод
+plt.show()
