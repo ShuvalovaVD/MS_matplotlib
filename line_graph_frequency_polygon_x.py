3a6028b8ea457f9dc20594271fa4bab2ac7e6ed9
Shuvalova Valeria <114807973+ShuvalovaVD@users.noreply.github.com>
2024-09-26 20:11:14 +0400

    Update line_graph_frequency_polygon_x.py

--- line_graph_frequency_polygon_x.py
+++ line_graph_frequency_polygon_x.py
@@ -4,7 +4,7 @@ from matplotlib.ticker import FixedLocator
 # набор данных
 x_all = [2.98, 4.95, 6.92, 8.89, 10.86, 12.83, 14.80, 16.77, 18.74, 20.71]
 y_all = [0.04, 0.06, 0.10, 0.10, 0.05, 0.04, 0.03, 0.04, 0.03, 0.03]
-# создание фигуры
+# создание фигуры, на к-рой будут размещаться графики (в нашем случае - один график)
 fig = plt.figure(figsize=(10, 6))
 p = fig.add_subplot()
 # линейный график
@@ -13,7 +13,7 @@ p.plot(x_all, y_all, color="red", marker="o")
 plt.title("Полигон частот для X")
 plt.ylabel("n_i/(n*w)")
 plt.xlabel("x")
-# сетка
+# отметки на осях с сеткой (нужны уникальные значения отметок, чтобы не было жирного наслоения на графике)
 x_all_unique, y_all_unique = list(set(x_all)), list(set(y_all))
 p.xaxis.set_major_locator(FixedLocator(x_all_unique))
 p.yaxis.set_major_locator(FixedLocator(y_all_unique))
