0cc79997b642c5e65bbc01d978312ffd8bf658f7
Shuvalova Valeria <114807973+ShuvalovaVD@users.noreply.github.com>
2024-09-26 20:17:20 +0400

    Update line_graph_sampling_distribution_function_x.py

--- line_graph_sampling_distribution_function_x.py
+++ line_graph_sampling_distribution_function_x.py
@@ -4,7 +4,7 @@ from matplotlib.ticker import FixedLocator
 # набор данных
 x_all = [2.98, 4.95, 6.92, 8.89, 10.86, 12.83, 14.80, 16.77, 18.74, 20.71]
 y_all = [0.07, 0.19, 0.39, 0.58, 0.67, 0.74, 0.80, 0.88, 0.94, 1.00]
-# создание фигуры
+# создание фигуры, на к-рой будут размещаться графики (в нашем случае - один график)
 fig = plt.figure(figsize=(10, 6))
 p = fig.add_subplot()
 # стрелочки
@@ -14,17 +14,17 @@ for i in range(len(x_all) - 1):
     x_line = [x_all[i], x_all[i + 1]]
     y_line = [y_all[i], y_all[i]]
     p.plot(x_line, y_line, color="red")
-# бесконечные линии влево и вправо (всё равно придется фотошопить)
+# ДОДЕЛАТЬ МОМЕНТ
 p.plot([x_all[0] - 2, x_all[0]], [0, 0], color="red")
 p.plot([x_all[-1], x_all[-1] + 2], [y_all[-1], y_all[-1]], color="red")
 # легенда
 plt.title("Выборочная функция распределения для X")
 plt.ylabel("F(x)")
 plt.xlabel("x")
-# сетка
-x_all_unique, y_all_unique = list(set(x_all)), list(set([0] + y_all))  # добавила 0 для оси Ox
+# отметки на осях с сеткой (нужны уникальные значения отметок, чтобы не было жирного наслоения на графике)
+x_all_unique, y_all_unique = list(set(x_all)), list(set([0] + y_all))  # ДОДЕЛАТЬ МОМЕНТ
 p.xaxis.set_major_locator(FixedLocator(x_all_unique))
 p.yaxis.set_major_locator(FixedLocator(y_all_unique))
-p.grid()
+p.grid()  # сетка
 # вывод
 plt.show()
