f74bf48e54b55482db37e57f4dda3482921c7b11
Shuvalova Valeria <114807973+ShuvalovaVD@users.noreply.github.com>
2024-09-27 09:29:44 +0400

    Update line_graph_sampling_distribution_function_y.py

--- line_graph_sampling_distribution_function_y.py
+++ line_graph_sampling_distribution_function_y.py
@@ -14,15 +14,19 @@ for i in range(len(x_all) - 1):
     x_line = [x_all[i], x_all[i + 1]]
     y_line = [y_all[i], y_all[i]]
     p.plot(x_line, y_line, color="red")
-# ДОДЕЛАТЬ МОМЕНТ
-p.plot([x_all[0] - 2, x_all[0]], [0, 0], color="red")
-p.plot([x_all[-1], x_all[-1] + 2], [y_all[-1], y_all[-1]], color="red")
+# осталось достроить граничные линии для крайних точек x при y = 0 и y = 1: продлеваем их на l1 единиц влево и вправо
+l1 = 3
+p.plot([x_all[0] - l1, x_all[0]], [0, 0], color="red")
+p.plot([x_all[-1], x_all[-1] + l1], [1, 1], color="red")
+# устанавливаем границы графика на l2 (l2 < l1) единиц слева и справа от крайних точек x, чтобы обрезать линии
+l2 = l1 - 1
+plt.xlim(x_all[0] - l2, x_all[-1] + l2)
 # легенда
 plt.title("Выборочная функция распределения для Y")
 plt.ylabel("F(y)")
 plt.xlabel("y")
 # отметки на осях с сеткой (нужны уникальные значения отметок, чтобы не было жирного наслоения на графике)
-x_all_unique, y_all_unique = list(set(x_all)), list(set([0] + y_all))  # ДОДЕЛАТЬ МОМЕНТ
+x_all_unique, y_all_unique = list(set(x_all)), list(set([0] + y_all))  # добавляем отметку y = 0, т. к. её нет в y_all
 p.xaxis.set_major_locator(FixedLocator(x_all_unique))
 p.yaxis.set_major_locator(FixedLocator(y_all_unique))
 p.grid()  # сетка
