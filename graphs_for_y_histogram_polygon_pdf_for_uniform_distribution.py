773baecc3aefbbb1d049056a5364a114cee0b729
Shuvalova Valeria <114807973+ShuvalovaVD@users.noreply.github.com>
2024-09-27 09:16:34 +0400

    Update graphs_for_y_histogram_polygon_pdf_for_uniform_distribution.py

--- graphs_for_y_histogram_polygon_pdf_for_uniform_distribution.py
+++ graphs_for_y_histogram_polygon_pdf_for_uniform_distribution.py
@@ -6,7 +6,6 @@ from matplotlib.ticker import FixedLocator
 
 # набор данных для гистограммы
 x_all_hist = [4.30, 8.48, 12.66, 16.84, 21.02, 25.20, 29.38, 33.56, 37.74, 41.92]  # коорд x, к-рые слева от столбиков
-x_left_hist = 4.30  # самый левый x
 x_right_hist = 46.10  # тот x, к-рый идёт после всех столбиков
 y_all_hist = [0.02, 0.03, 0.04, 0.03, 0.02, 0.01, 0.03, 0.03, 0.01, 0.02]  # знач столбиков по y, соответствуют x_all
 width_hist = x_all_hist[1] - x_all_hist[0]  # ширина интервала
@@ -26,13 +25,14 @@ p.plot(x_all_pol, y_all_pol, color="red", marker="o", label='полигон')
 # ПРВ равномерного распределения
 a, b = 4.30, 46.10  # параметры a и b
 f_val = round(1 / (b - a), 2)  # 1 / (b - a), округленное до 2-х знаков после точки
-p.plot([a, b], [f_val, f_val], color="green", linewidth=2.0, marker="o",
-       label="ПРВ равномерного распределения")
-# ДОДЕЛАТЬ ЭТОТ МОМЕНТ
-p.plot([a - 3, a - 0.30], [0, 0], linewidth=4.0, color="green")
-p.scatter([a], [0.0004], marker="o", linewidth=1.5, color="white", edgecolors="black", s=50)
-p.plot([b + 0.38, b + 3], [0, 0], linewidth=4.0, color="green")
-p.scatter([b], [0.0004], marker="o", linewidth=1.5, color="white", edgecolors="black", s=50)
+p.plot([a, b], [f_val, f_val], color="green", linewidth=2.0, marker="o", label="ПРВ равномерного распределения")
+# продлеваем линии на l1 единиц слева и справа от границ гистограммы
+l1 = 3
+p.plot([a - l1, a], [0, 0], linewidth=4.0, color="green", marker="o", markerfacecolor="white", markersize=7)
+p.plot([b, b + l1], [0, 0], linewidth=4.0, color="green", marker="o", markerfacecolor="white", markersize=7)
+# теперь устанавливаем границы графика на l2 (l2 < l1) единиц слева и справа от границ гистограммы, чтобы обрезать линии
+l2 = l1 - 1
+plt.xlim(x_all_hist[0] - l2, x_right_hist + l2)
 
 # легенда
 plt.title("Графики для Y: гистограмма, полигон, ПРВ для равномерного распределения")
